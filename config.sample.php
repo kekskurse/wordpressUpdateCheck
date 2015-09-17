baa362e2072133954c4ef452b8003f3840f77aa7
Soeren <soeren@siegismund-poschmann.de>
2015-09-17 12:26:54 +0200

    Add Sample Config

---
+++ config.sample.php
@@ -0,0 +1,10 @@
+<?php
+$url = "http://blog.kekskurse.de";
+$user = "username";
+$pass = "password";
+ 
+$tofind[] = "Du benutzt die aktuelle Version von WordPress";
+$tofind[] = "Alle Plugins sind auf dem neuesten Stand.";
+$tofind[] = "Alle Themes sind auf dem neuesten Stand.";
+$tofind[] = "Deine Übersetzungen sind alle auf dem neuesten Stand.";
+
